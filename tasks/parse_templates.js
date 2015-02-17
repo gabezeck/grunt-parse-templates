f87a3d26a9178324467e8e6809eb723ad321f31f
Gabriel Zeck <gabriel.zeck@vimandvigor.co>
2015-02-17 14:56:54 -0600

    Added properties for URL segments that Angular can use for routing.

--- tasks/parse_templates.js
+++ tasks/parse_templates.js
@@ -15,26 +15,31 @@ module.exports = function(grunt) {
         var templatedir = grunt.config.get('parse_templates.filedir'),
             datadir = grunt.config.get('parse_templates.datadir'),
             catArr = [],
-            manString = function(string){
+            nameString = function(string){
                return string.replace(/^.*[\\\/]/, '').replace(/_/g, ' ').replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
             };
 
         grunt.file.expand({filter: 'isDirectory'}, templatedir + '*').forEach(function(val) {
 
-            var catObj = {
-                sectionName: manString(val),
-                pages: []
-            };
+            var urlSegs = val.split('/'),
+                catObj = {
+                    sectionName: nameString(val),
+                    urlString: urlSegs[urlSegs.length - 1],
+                    pages: []
+                };
+
             grunt.file.expand({filter: 'isDirectory'}, val + '/*').forEach(function(subdir,i) {
 
                 catObj.pages[i] = {
                     pageName: "",
+                    pageUrl: "",
                     files: []
                 };
 
                 grunt.file.recurse(subdir, function (rootdir) {
                     var dirSegs = rootdir.split('/');
-                    catObj.pages[i].pageName = manString(dirSegs[dirSegs.length - 2]);
+                    catObj.pages[i].pageName = nameString(dirSegs[dirSegs.length - 2]);
+                    catObj.pages[i].pageUrl = dirSegs[dirSegs.length - 2];
                     catObj.pages[i].files.push(rootdir);
                 });
 
