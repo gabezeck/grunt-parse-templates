36f38910c78459a4d9aa611d87b79a27be8baff4
Gabriel Zeck <gabriel.zeck@vimandvigor.co>
2015-02-26 11:49:51 -0600

    Update to Config
    Made the appData object an application constant so that it is accessible across the application.

--- test/data/config.js
+++ test/data/config.js
@@ -1,45 +1,45 @@
-StyleGuideApp.config(['$stateProvider', function($stateProvider) {
-    var appData = {
-        "sections": [{
-            "sectionName": "Components",
-            "urlString": "components",
-            "sectionTemplate": "test/templates/base.html",
-            "pageTemplate": "test/templates/components/page.html",
-            "pages": [{
-                "pageName": "Buttons",
-                "pageUrl": "buttons",
-                "files": ["test/templates/components/buttons/buttons.html"]
-            }, {
-                "pageName": "Footers",
-                "pageUrl": "footers",
-                "files": ["test/templates/components/footers/footers.html"]
-            }]
+StyleGuideApp.constant('appData', {
+    "sections": [{
+        "sectionName": "Components",
+        "urlString": "components",
+        "sectionTemplate": "test/templates/base.html",
+        "pageTemplate": "test/templates/components/page.html",
+        "pages": [{
+            "pageName": "Buttons",
+            "pageUrl": "buttons",
+            "files": ["test/templates/components/buttons/buttons.html"]
         }, {
-            "sectionName": "Layouts",
-            "urlString": "layouts",
-            "sectionTemplate": "test/templates/layouts/section.html",
-            "pageTemplate": "test/templates/layouts/page.html",
-            "pages": [{
-                "pageName": "Profile",
-                "pageUrl": "profile",
-                "files": ["test/templates/layouts/profile/profile-image.html"]
-            }]
-        }, {
-            "sectionName": "Modules",
-            "urlString": "modules",
-            "sectionTemplate": "test/templates/modules/section.html",
-            "pageTemplate": "test/templates/modules/page.html",
-            "pages": [{
-                "pageName": "Boxes",
-                "pageUrl": "boxes",
-                "files": ["test/templates/modules/boxes/base-boxes.html", "test/templates/modules/boxes/metric-boxes.html"]
-            }, {
-                "pageName": "Forms",
-                "pageUrl": "forms",
-                "files": ["test/templates/modules/forms/headers.html"]
-            }]
+            "pageName": "Footers",
+            "pageUrl": "footers",
+            "files": ["test/templates/components/footers/footers.html"]
         }]
-    };
+    }, {
+        "sectionName": "Layouts",
+        "urlString": "layouts",
+        "sectionTemplate": "test/templates/layouts/section.html",
+        "pageTemplate": "test/templates/layouts/page.html",
+        "pages": [{
+            "pageName": "Profile",
+            "pageUrl": "profile",
+            "files": ["test/templates/layouts/profile/profile-image.html"]
+        }]
+    }, {
+        "sectionName": "Modules",
+        "urlString": "modules",
+        "sectionTemplate": "test/templates/modules/section.html",
+        "pageTemplate": "test/templates/modules/page.html",
+        "pages": [{
+            "pageName": "Boxes",
+            "pageUrl": "boxes",
+            "files": ["test/templates/modules/boxes/base-boxes.html", "test/templates/modules/boxes/metric-boxes.html"]
+        }, {
+            "pageName": "Forms",
+            "pageUrl": "forms",
+            "files": ["test/templates/modules/forms/headers.html"]
+        }]
+    }]
+});
+StyleGuideApp.config(['$stateProvider', 'appData', function($stateProvider, appData) {
     angular.forEach(appData.sections, function(sections, i) {
         $stateProvider.state(sections.urlString, {
             url: '/' + sections.urlString,
